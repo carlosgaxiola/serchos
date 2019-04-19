37fad05b38f39d91ff64054ef126e4c7c411b4d8
carlosgaxiola <2016030023@upsin.edu.mx>
2019-04-18 23:13:51 -0600

    Update MY_Model.php

--- application/core/MY_Model.php
+++ application/core/MY_Model.php
@@ -3,69 +3,56 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class MY_Model extends CI_Model {
 
+	private $tabla;
+	private $view;
+
 	public function __construct() {
 		parent::__construct();
 	}
 
-	public function listar ($tabla, $extras = null, $status = -1) {	
-		if ($extras != null)
-			$this->db->select(implode(",", $extras));
-		if ($status != -1)
-			$this->db->where($tabla.".status", $status);
-		$this->db->select($tabla.".*");
-		$res = $this->db->get($tabla);		
+	public function listar ($where = -1, $order = -1, $start = 0, $limit = 100) {	
+		if (is_array($where))
+			foreach ($where as $index => $value)
+				$this->db->where($index, $value);
+		if (is_array($order))
+			foreach ($order as $index => $value)
+				$this->db->order_by($index, $value);
+		$this->db->limit($start, $limit);
+		$res = $this->db->get($this->view);
 		if ($res->num_rows() > 0)
 			return $res->result_array();
 		return false;
 	}
 
-	public function insertar ($tabla, $data) {
-		$this->db->insert($tabla, $data);		
+	public function insertar ($data) {
+		$this->db->insert($this->tabla, $data);		
 		if ($this->db->affected_rows() > 0)
 			return $this->db->insert_id();
 		return false;
 	}
 
-	public function actualizar ($tabla, $id, $data, $campo = 'id') {
-		if (is_array($id))
-			foreach ($id as $index => $value)
-				$this->db->where($index, $value);			
-		else
-			$this->db->where($campo, $id);
-		$this->db->update($tabla, $data);
-		return ($this->db->affected_rows() > 0);		
-	}
-
-	public function buscar ($tabla, $valor, $campo = 'id') {
-		if (is_array($valor))
-			foreach ($valor as $index => $value)
+	public function actualizar ($data, $where, $field = 'id') {
+		if (is_array($where))
+			foreach ($where as $index => $value)
 				$this->db->where($index, $value);
 		else
-			$this->db->where($campo, $valor);			
-		$res = $this->db->get($tabla);		
-		if ($res->num_rows() == 1)
-			return $res->row_array();
-		else if ($res->num_rows() > 1)
-			return $res->result_array();
+			$this->db->where($field, $where);
+		$this->db->update($this->tabla, $data);
+		return ($this->db->affected_rows() > 0);
+	}
+
+	public function buscar ($where) {
+		$result = $this->listar($where);
+		return $result[0];
+	}
+
+	public function borrar ($where = -1) {
+		if (is_array($where)) {
+			foreach ($where as $index => $value)
+				$this->db->where($index, $value);
+			$this->db->delete($this->tabla);
+			return ($this->db->affected_rows() > 0);
+		}
 		return false;
-	}
-
-	public function alternar ($tabla, $id, $status) {
-		$this->db->where("id", $id);
-		$this->db->set("status", $status != 1);
-		$this->db->update($tabla);
-		return ($this->db->affected_rows() > 0);
-	}
-
-	public function join ($tabla, $join, $tipo = 0) {
-		if ($tipo != 0)	$this->db->join($tabla, $join, $tipo);
-		else $this->db->join($tabla, $join);
-		return $this;
-	}
-
-	public function borrar ($tabla, $valor, $campo = 'id') {
-		$this->db->where($campo, $valor);
-		$this->db->delete($tabla);
-		return ($this->db->affected_rows() > 0);
 	}
 }
